95219168801bf39b618d139fbfede270977e84f6
sugnani123 <91664490+sugnani123@users.noreply.github.com>
2021-11-16 12:59:36 +0530

    Update main.tf

--- app-service-variables/main.tf
+++ app-service-variables/main.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 provider "azurerm" {
-  version = ">=2.0"
-  # The "feature" block is required for AzureRM provider 2.x.
+#   version = ">=2.0"
+#   # The "feature" block is required for AzureRM provider 2.x.
   features {}
 }
 
