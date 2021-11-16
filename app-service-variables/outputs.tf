a139d160a363f704bdde1b112c54762e90a18e35
sugnani123 <91664490+sugnani123@users.noreply.github.com>
2021-11-16 13:12:17 +0530

    Update outputs.tf

--- app-service-variables/outputs.tf
+++ app-service-variables/outputs.tf
@@ -2,6 +2,6 @@ output "app_service_name" {
   value = azurerm_app_service.test.name
 }
 
-output "app_service_default_hostname" {
-  value = "https://${azurerm_app_service.test.default_site_hostname}"
-}
+# output "app_service_default_hostname" {
+#   value = "https://${azurerm_app_service.test.default_site_hostname}"
+# }
